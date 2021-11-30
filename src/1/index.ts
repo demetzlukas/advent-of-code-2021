900fba24bc3b9429f425491b0677c95d5cc99b49
Lukas Demetz <lukas.demetz@fh-kufstein.ac.at>
2021-11-30 21:51:43 +0100

    Init

---
+++ src/1/index.ts
@@ -0,0 +1,7 @@
+import { readLinesFromInput } from '../utils/readFile';
+
+const filename = './input/1.txt';
+
+export async function main() {
+  const content = await readLinesFromInput(filename);
+}
