4f577ff8c606004f55ed2e2144100da55b82dfc2
Lukas Demetz <lukas.demetz@fh-kufstein.ac.at>
2021-12-03 08:12:14 +0100

    Solution day 3

---
+++ src/3/index.ts
@@ -0,0 +1,54 @@
+import { readLinesFromInput } from '../utils/readFile';
+
+const filename = './input/3.txt';
+
+export async function main() {
+  const input = await readLinesFromInput(filename);
+
+  let epsilon = '';
+  let gamma = '';
+
+  for (let index = 0; index < input[0].length; index++) {
+    let counter = 0;
+
+    for (const line of input) {
+      counter = line[index] === '0' ? counter - 1 : counter + 1;
+    }
+
+    if (counter > 0) {
+      epsilon += '1';
+      gamma += '0';
+    } else {
+      epsilon += '0';
+      gamma += '1';
+    }
+  }
+
+  console.log('Part 1:', parseInt(epsilon, 2) * parseInt(gamma, 2));
+
+  const oxygen = calculateValue(input, (counter) => (counter > -1 ? '1' : '0'));
+  const co2 = calculateValue(input, (counter) => (counter > -1 ? '0' : '1'));
+  console.log('Part 2:', oxygen * co2);
+}
+
+function calculateValue(
+  input: string[],
+  getBit: (counter: number) => string
+): number {
+  let result = input;
+
+  for (let index = 0; index < result[0].length; index++) {
+    let counter = 0;
+
+    for (const line of result) {
+      counter = line[index] === '0' ? counter - 1 : counter + 1;
+    }
+    result = result.filter((line) => line[index] === getBit(counter));
+
+    if (result.length === 1) {
+      break;
+    }
+  }
+
+  return parseInt(result[0], 2);
+}
